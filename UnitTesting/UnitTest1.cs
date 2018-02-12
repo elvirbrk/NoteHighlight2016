62c4415bbaa751c69a84beb9800e5db660c3975b
Elvir Brkić <elvir@outlook.com>
2018-02-12 16:33:57 +0100

    Initial Test skeleton

--- UnitTesting/UnitTest1.cs
+++ UnitTesting/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NoteHighlightAddin;
+using GenerateHighlightContent;
+using System.Xml.Linq;
 
 namespace UnitTesting
 {
@@ -9,6 +12,15 @@ namespace UnitTesting
         [TestMethod]
         public void TestMethod1()
         {
+            string htmlCode = "";
+            string[] pos = new string[] { "", "" };
+            HighLightParameter param = new HighLightParameter();
+            XElement outline = new XElement("");
+            
+
+            AddIn addIn = new AddIn();
+
+            addIn.InsertHighLightCode(htmlCode, pos, param, outline, false, false);
         }
     }
 }
