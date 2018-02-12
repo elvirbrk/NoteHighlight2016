26d42a7a05ee4ad1df4e39e76ece51812c546e46
Elvir Brkić <elvir@outlook.com>
2018-02-12 16:19:34 +0100

    Added unit test project

---
+++ UnitTesting/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
