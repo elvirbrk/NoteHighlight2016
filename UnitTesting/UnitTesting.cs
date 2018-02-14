55512bf379bca9d78fcf425a82d0fd22d72a8d9d
Elvir Brkić <elvir@outlook.com>
2018-02-14 13:59:30 +0100

    Added test for selected code formatting

--- UnitTesting/UnitTesting.cs
+++ UnitTesting/UnitTesting.cs
@@ -4,6 +4,7 @@ using NoteHighlightAddin;
 using GenerateHighlightContent;
 using System.Xml.Linq;
 using System.Configuration;
+using System.Linq;
 
 namespace UnitTesting
 {
@@ -35,6 +36,35 @@ namespace UnitTesting
             XDocument output = addIn.InsertHighLightCode(htmlCode, pos, param, outline, config, false, false);
 
             Assert.AreEqual(Resource1.Output1, output.ToString(), false);
+        }
+
+        [TestMethod]
+        public void FormatSelectedCode_AllSelected()
+        {
+            string htmlCode = Resource1.HTMLContent2;
+            string[] pos = null;
+            HighLightParameter param = new HighLightParameter();
+            param.ShowLineNumber = true;
+            param.HighlightColor = System.Drawing.Color.FromArgb(240, 240, 240);
+
+
+            AddIn addIn = new AddIn();
+            addIn.ns = @"http://schemas.microsoft.com/office/onenote/2013/onenote";
+
+            var outline = XDocument.Parse(Resource1.Page2).Descendants(addIn.ns + "Outline")
+                                   .Where(n => n.Attribute("selected") != null && (n.Attribute("selected").Value == "all" || n.Attribute("selected").Value == "partial"))
+                                   .FirstOrDefault();
+
+            //Arrange
+            ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap { ExeConfigFilename = "Test.config" };
+            Configuration configuration = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+
+            HighLightSection config = configuration.GetSection("HighLightSection") as HighLightSection;
+
+
+            XDocument output = addIn.InsertHighLightCode(htmlCode, pos, param, outline, config, false, false);
+
+            Assert.AreEqual(Resource1.Output2, output.ToString(), false);
         }
 
         [TestMethod]
