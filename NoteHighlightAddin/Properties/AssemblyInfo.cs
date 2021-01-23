d83187dc5ea66b7ca02cf351e4a0de3275f3a93e
Elvir <elvir@outlook.com>
2021-01-23 17:43:04 +0100

    Updated version to 3.7

--- NoteHighlightAddin/Properties/AssemblyInfo.cs
+++ NoteHighlightAddin/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.6.*")]
-[assembly: AssemblyFileVersion("3.6.*")]
+[assembly: AssemblyVersion("3.7.*")]
+[assembly: AssemblyFileVersion("3.7.*")]
